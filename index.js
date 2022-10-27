2f5684b48e86d81971d4ae3be6aec7b4f4479884
gutochiele <63303276+gutochiele@users.noreply.github.com>
2022-10-27 18:57:22 +0200

    added if win and lose func

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-//GET A FUNCTION THAT MAKES THE COMPUTER RETURN RANDOMLY "ROCK", "PAPER" or "SCISSOR"
-
-function getComputerChoice(list){
-    return list[Math.floor(Math.random()*list.length)];
+function getComputerChoice(compRandom){
+    return compRandom[Math.floor(Math.random()*compRandom.lenght)];
 }
-    const list = (["rock", "paper", "scissor"])
+    const compRandom = (["ROCK", "PAPER", "SCISSORS"])
 
+    let computerSelection = getComputerChoice(compRandom);
+
+console.log(computerSelection)
+
+    function playRound(playerSelection, computerSelection){
+    let win = `You win! ${playerSelection} beats ${computerSelection}!`;
+    let lose = `Oh no! ${computerSelection} beats ${playerSelection}!`;
+    let draw = "It's a draw!";
+    playerSelection === playerSelection.toUpperCase();
+    if (playerSelection = computerSelection){
+        return draw;
+    }   else if (playerSelection = "ROCK", computerSelection = "SCISSORS"){
+        return win;
+    }   else if (playerSelection = "ROCK", computerSelection = "PAPER"){
+        return lose;
+    }   else if (playerSelection = "PAPER", computerSelection = "ROCK"){
+        return win;
+    }   else if (playerSelection = "PAPER", computerSelection = "SCISSORS"){
+        return lose;
+    }   else if (playerSelection = "SCISSORS", computerSelection = "ROCK"){
+        return lose;
+    }   else if (playerSelection = "SCISSORS", computerSelection = "PAPER"){
+        return win;
+    }
+}
+
+   
+  const playerSelection = "Paper";
+  console.log(playRound(playerSelection, computerSelection));
