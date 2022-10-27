5d590a473aaab1708764bd4bab7a1170d7d3c593
gutochiele <63303276+gutochiele@users.noreply.github.com>
2022-10-27 17:29:14 +0200

    First function completed  return random from comp

---
+++ index.js
@@ -0,0 +1,7 @@
+//GET A FUNCTION THAT MAKES THE COMPUTER RETURN RANDOMLY "ROCK", "PAPER" or "SCISSOR"
+
+function getComputerChoice(list){
+    return list[Math.floor(Math.random()*list.length)];
+}
+    const list = (["rock", "paper", "scissor"])
+
