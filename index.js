fe7e1e79c38984ee71ed12d8c942a372aace7d88
gutochiele <63303276+gutochiele@users.noreply.github.com>
2022-10-27 19:02:44 +0200

    Fixed undefined bug

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 function getComputerChoice(compRandom){
-    return compRandom[Math.floor(Math.random()*compRandom.lenght)];
+    return compRandom[Math.floor(Math.random()*compRandom.length)];
 }
-    const compRandom = (["ROCK", "PAPER", "SCISSORS"])
+    const compRandom = (["rock", "paper", "scissor"])
 
     let computerSelection = getComputerChoice(compRandom);
 
 console.log(computerSelection)
 
-    function playRound(playerSelection, computerSelection){
-    let win = `You win! ${playerSelection} beats ${computerSelection}!`;
-    let lose = `Oh no! ${computerSelection} beats ${playerSelection}!`;
-    let draw = "It's a draw!";
-    playerSelection === playerSelection.toUpperCase();
-    if (playerSelection = computerSelection){
-        return draw;
-    }   else if (playerSelection = "ROCK", computerSelection = "SCISSORS"){
-        return win;
-    }   else if (playerSelection = "ROCK", computerSelection = "PAPER"){
-        return lose;
-    }   else if (playerSelection = "PAPER", computerSelection = "ROCK"){
-        return win;
-    }   else if (playerSelection = "PAPER", computerSelection = "SCISSORS"){
-        return lose;
-    }   else if (playerSelection = "SCISSORS", computerSelection = "ROCK"){
-        return lose;
-    }   else if (playerSelection = "SCISSORS", computerSelection = "PAPER"){
-        return win;
-    }
-}
+//     function playRound(playerSelection, computerSelection){
+//     let win = `You win! ${playerSelection} beats ${computerSelection}!`;
+//     let lose = `Oh no! ${computerSelection} beats ${playerSelection}!`;
+//     let draw = "It's a draw!";
+//     playerSelection === playerSelection.toUpperCase();
+//     if (playerSelection = computerSelection){
+//         return draw;
+//     }   else if (playerSelection = "ROCK", computerSelection = "SCISSORS"){
+//         return win;
+//     }   else if (playerSelection = "ROCK", computerSelection = "PAPER"){
+//         return lose;
+//     }   else if (playerSelection = "PAPER", computerSelection = "ROCK"){
+//         return win;
+//     }   else if (playerSelection = "PAPER", computerSelection = "SCISSORS"){
+//         return lose;
+//     }   else if (playerSelection = "SCISSORS", computerSelection = "ROCK"){
+//         return lose;
+//     }   else if (playerSelection = "SCISSORS", computerSelection = "PAPER"){
+//         return win;
+//     }
+// }
 
    
-  const playerSelection = "Paper";
-  console.log(playRound(playerSelection, computerSelection));
+//   const playerSelection = "Paper";
+//   console.log(playRound(playerSelection, computerSelection));
