73f65dcce86b8521be63ecac359bac75a65988e7
gutochiele <63303276+gutochiele@users.noreply.github.com>
2022-10-27 19:10:10 +0200

    Fixed upper case display on console

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 function getComputerChoice(compRandom){
     return compRandom[Math.floor(Math.random()*compRandom.length)];
 }
-    const compRandom = (["rock", "paper", "scissor"])
+    const compRandom = (["ROCK", "PAPER", "SCISSORS"])
 
     let computerSelection = getComputerChoice(compRandom);
 
-console.log(computerSelection)
+console.log("Computer draws: " + computerSelection)
 
-//     function playRound(playerSelection, computerSelection){
-//     let win = `You win! ${playerSelection} beats ${computerSelection}!`;
-//     let lose = `Oh no! ${computerSelection} beats ${playerSelection}!`;
-//     let draw = "It's a draw!";
-//     playerSelection === playerSelection.toUpperCase();
-//     if (playerSelection = computerSelection){
-//         return draw;
-//     }   else if (playerSelection = "ROCK", computerSelection = "SCISSORS"){
-//         return win;
-//     }   else if (playerSelection = "ROCK", computerSelection = "PAPER"){
-//         return lose;
-//     }   else if (playerSelection = "PAPER", computerSelection = "ROCK"){
-//         return win;
-//     }   else if (playerSelection = "PAPER", computerSelection = "SCISSORS"){
-//         return lose;
-//     }   else if (playerSelection = "SCISSORS", computerSelection = "ROCK"){
-//         return lose;
-//     }   else if (playerSelection = "SCISSORS", computerSelection = "PAPER"){
-//         return win;
-//     }
-// }
+    function playRound(playerSelection, computerSelection){
+    let win = `You win! ${playerSelection} beats ${computerSelection}!`;
+    let lose = `Oh no! You lose! ${computerSelection} beats ${playerSelection}!`;
+    let draw = "It's a draw!";
+
+
+    if (playerSelection === computerSelection){
+        return draw;
+    }   else if (playerSelection === "ROCK", computerSelection === "SCISSORS"){
+        return win;
+    }   else if (playerSelection === "ROCK", computerSelection === "PAPER"){
+        return lose;
+    }   else if (playerSelection === "PAPER", computerSelection === "ROCK"){
+        return win;
+    }   else if (playerSelection === "PAPER", computerSelection === "SCISSORS"){
+        return lose;
+    }   else if (playerSelection === "SCISSORS", computerSelection === "ROCK"){
+        return lose;
+    }   else if (playerSelection === "SCISSORS", computerSelection === "PAPER"){
+        return win;
+    }
+}
 
    
-//   const playerSelection = "Paper";
-//   console.log(playRound(playerSelection, computerSelection));
+  let playerSelection = "Paper";
+  playerSelection = playerSelection.toUpperCase();
+  console.log(playRound(playerSelection, computerSelection));
