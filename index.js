b62ac3768898d7c68d5cfe5193a43823f70b5f31
gutochiele <63303276+gutochiele@users.noreply.github.com>
2022-10-28 13:15:29 +0200

    still trying

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let playerScore = 0;
 let compScore = 0;
 let rounds = 0;
 
+console.log("The game begins!") 
+
+
+let playerSelection = prompt("Lets play ROCK, PAPER, SCISSORS\nPut your choice below:");
+playerSelection = playerSelection.toUpperCase();
+
+
 function getComputerChoice(compRandom){
     return compRandom[Math.floor(Math.random()*compRandom.length)];
 }
@@ -11,42 +18,47 @@ function getComputerChoice(compRandom){
 
 
     function playRound(playerSelection, computerSelection){
-    let win = `You win! ${playerSelection} beats ${computerSelection}!`;
-    let lose = `Oh no! You lose! ${computerSelection} beats ${playerSelection}!`;
-    let draw = "It's a draw!";
-
+    // let win = 'win';
+    // let lose = 'lose';
+    // let draw = 'draw';
+    // const result = ([lose, win, draw]);
 
     if (playerSelection === computerSelection){
         return draw;
     }   else if (playerSelection === "ROCK", computerSelection === "SCISSORS"){
-        return win, playerScore++;
+        return playerScore++, win;
     }   else if (playerSelection === "ROCK", computerSelection === "PAPER"){
-        return lose, compScore++;
+        return compScore++, lose;
     }   else if (playerSelection === "PAPER", computerSelection === "ROCK"){
-        return win, playerScore++;
+        return playerScore++, win;
     }   else if (playerSelection === "PAPER", computerSelection === "SCISSORS"){
-        return lose, compScore++;
+        return compScore++, lose;
     }   else if (playerSelection === "SCISSORS", computerSelection === "ROCK"){
-        return lose, compScore++;
+        return compScore++, lose;
     }   else if (playerSelection === "SCISSORS", computerSelection === "PAPER"){
-        return win, playerScore++;
-    }
+        return playerScore++, win;
+}
 }
 
-
 // function game(){
-//     console.log("The game begins!")
-//     for (rounds = 0; rounds < 5; rounds++){
-//         playRound(playerSelection, computerSelection); 
-
-//     alert("Computer draws: " + computerSelection)
-//     alert("You chose: " + playerSelection)
-
-//     }
-//     console.log(" Player Score: " + playerScore);
-//     console.log("Computer Score: " + compScore);
-// }
-
-// let playerSelection = prompt("Lets play ROCK, PAPER, SCISSORS\nPut your choice below:");
-// playerSelection = playerSelection.toUpperCase();
     
+    // for (rounds = 0; rounds < 5; rounds++){
+
+        playRound(playerSelection, computerSelection);{ 
+
+        console.log("Computer draws: " + computerSelection);
+        console.log("You chose: " + playerSelection);
+        if (draw) {
+            console.log("It's a draw!");
+            
+        } else if (lose){
+            console.log(`Oh no! You lose! ${computerSelection} beats ${playerSelection}!`);
+           
+        } else if (win){
+            console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+            
+        }
+
+        console.log(" Player Score: " + playerScore);
+        console.log("Computer Score: " + compScore);
+    }
