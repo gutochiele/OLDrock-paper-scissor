6343f7b5bf0566cc51b6f7685940d3d5ba0f2557
gutochiele <63303276+gutochiele@users.noreply.github.com>
2022-10-31 16:28:35 +0100

    added Do While loop to test

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ let playerScore = 0;
 let compScore = 0;
 let rounds = 0;
 
-// console.log("The game begins!") 
 
+// let playerSelection = prompt("Lets play ROCK, PAPER, SCISSORS\nPut your choice below:");
+// playerSelection = playerSelection.toUpperCase();
 
-let playerSelection = prompt("Lets play ROCK, PAPER, SCISSORS\nPut your choice below:");
+let playerSelection;
+
+while (true) {
+    playerSelection = prompt("Lets play ROCK, PAPER, SCISSORS\nPut your choice below:");
+    if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"){
+        break;
+    }
+    alert("Sorry, I didn't understand that! Try again:");
+}
 playerSelection = playerSelection.toUpperCase();
-
 
 function getComputerChoice(compRandom){
     return compRandom[Math.floor(Math.random()*compRandom.length)];
@@ -19,43 +27,42 @@ function getComputerChoice(compRandom){
     let win = `You win! ${playerSelection} beats ${computerSelection}!`;
     let lose = `Oh no! You lose! ${computerSelection} beats ${playerSelection}!`;
     let draw = "It's a draw!";
+    let playerError = "I'm sorry, I didn't understand that!"
+
 
     function playRound(playerSelection, computerSelection){     
-
-    if (playerSelection === computerSelection){
-        return draw, rounds++, console.log(draw);
-    }   else if (playerSelection === "ROCK", computerSelection === "SCISSORS"){
-        return win, playerScore++, rounds++, console.log(win);
-    }   else if (playerSelection === "ROCK", computerSelection === "PAPER"){
-        return lose, compScore++, rounds++, console.log(lose);
-    }   else if (playerSelection === "PAPER", computerSelection === "ROCK"){
-        return win, playerScore++, rounds++, console.log(win);
-    }   else if (playerSelection === "PAPER", computerSelection === "SCISSORS"){
-        return lose, compScore++, rounds++, console.log(lose);
-    }   else if (playerSelection === "SCISSORS", computerSelection === "ROCK"){
-        return lose, compScore++, rounds++, console.log(lose);
-    }   else if (playerSelection === "SCISSORS", computerSelection === "PAPER"){
-        return win, playerScore++, rounds++, console.log(win);
-    }   else if (playerSelection !== ["ROCK", "PAPER", "SCISSORS"]){
-        console.log("Sorry, I didn't understand that!");
-    }
-
+ 
+        if (playerSelection === computerSelection){
+            return draw, rounds++, console.log(draw);
+        }   else if (playerSelection === "ROCK", computerSelection === "SCISSORS"){
+            return win, playerScore++, rounds++, console.log(win);
+        }   else if (playerSelection === "ROCK", computerSelection === "PAPER"){
+            return lose, compScore++, rounds++, console.log(lose);
+        }   else if (playerSelection === "PAPER", computerSelection === "ROCK"){
+            return win, playerScore++, rounds++, console.log(win);
+        }   else if (playerSelection === "PAPER", computerSelection === "SCISSORS"){
+            return lose, compScore++, rounds++, console.log(lose);
+        }   else if (playerSelection === "SCISSORS", computerSelection === "ROCK"){
+            return lose, compScore++, rounds++, console.log(lose);
+        }   else if (playerSelection === "SCISSORS", computerSelection === "PAPER"){
+            return win, playerScore++, rounds++, console.log(win);
+        // }   else if (playerSelection !== "ROCK" || playerSelection !== "PAPER" || playerSelection !== "SCISSORS"){
+        //     return playerError, console.log(playerError);
+        }
 }
 
-// function game(){
-    
-    // for (rounds = 0; rounds < 5; rounds++){
+ 
 
-        playRound(playerSelection, computerSelection);{ 
+        do {
 
+            playRound(playerSelection, computerSelection);{ 
+                
+            console.log("You chose: " + playerSelection);
+            console.log("Computer chose: " + computerSelection);
 
-            
-        console.log("You chose: " + playerSelection);
-        console.log("Computer chose: " + computerSelection);
-
-
-
-        console.log(" Player Score: " + playerScore);
-        console.log("Computer Score: " + compScore);
-        console.log(`It's the round number ${rounds}!`)
-    }
+            console.log(" Player Score: " + playerScore);
+            console.log("Computer Score: " + compScore);
+            console.log(`It's the round number ${rounds}!`)
+            }
+        }
+        while (rounds < 6);
